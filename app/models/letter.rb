300f37b8c5d247e78b6ba254a99279bac7fe0be5
zauzaj <jordanovskibojan@gmail.com>
2015-09-23 11:26:20 +0200

    Cache top 10 records with Redis

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,5 +1,7 @@
+require_relative "../../lib/letter_redis_repository"
 class Letter < ActiveRecord::Base
   def self.top_10
-    limit(10000).order(score: :desc)
+    ids = LetterRedisRepository.top
+    where(id: ids)
   end
 end
